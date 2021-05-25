0b45f2b222715a69499151ef34a250bc2fef7d17
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-25 18:02:22 -0400

    black again

--- app/cogs/information.py
+++ app/cogs/information.py
@@ -24,38 +24,38 @@ class Information(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(
-        name='ping',
-        aliases=('latency', 'lat', 'ms'),
-        brief='Pong !'
+        name="ping", aliases=("latency", "lat", "ms"), brief="Pong !"
     )
     @commands.cooldown(2, 60, commands.BucketType.user)
     async def ping(self, ctx):
-        """ Get the latency of the client converted in milliseconds.
+        """Get the latency of the client converted in milliseconds.
         An dynamically colored ball will show in the image in function of the ping.
-        Give also worst, best and daily average ping """
+        Give also worst, best and daily average ping"""
         latencies: dict = {"API": self.bot.latency}
 
         marker: float = perf_counter()
-        ping_message = await ctx.send('> pinging...')
+        ping_message = await ctx.send("> pinging...")
         latencies["BOT"] = perf_counter() - marker
 
         _embed = self.bot.embed(
-            title=f'{ctx.author.name} Ponged !',
-            description="The bot, bd & API latency"
+            title=f"{ctx.author.name} Ponged !",
+            description="The bot, bd & API latency",
         )
 
         for k, v in latencies.items():
-            _embed.add_field(name=f"{k} latency", value=f'> `{v * 1e3:,.2f} ms`')
+            _embed.add_field(
+                name=f"{k} latency", value=f"> `{v * 1e3:,.2f} ms`"
+            )
 
-        await ping_message.edit(content=' ', embed=_embed)
+        await ping_message.edit(content=" ", embed=_embed)
 
     @commands.command(
         name="user",
         aliases=("whois", "more"),
-        brief="return the user information"
+        brief="return the user information",
     )
     async def user(self, ctx, user: discord.Member = None):
-        """ return the user information """
+        """return the user information"""
         if user is None:
             user = ctx.author
 
@@ -63,8 +63,9 @@ class Information(commands.Cog):
         embed.set_thumbnail(url=user.avatar_url)
 
         embed.set_author(
-            name=f"{user.name}%s Information" % ("'" if user.name.endswith("s") else "'s"),
-            icon_url=user.avatar_url
+            name=f"{user.name}%s Information"
+            % ("'" if user.name.endswith("s") else "'s"),
+            icon_url=user.avatar_url,
         )
 
         if user.nick is not None:
@@ -72,51 +73,52 @@ class Information(commands.Cog):
 
         for name, value in {
             "default avatar": user.default_avatar,
-            "platform": 'Mobile' if user.is_on_mobile() else 'computer',
+            "platform": "Mobile" if user.is_on_mobile() else "computer",
             "status": user.status,
             "discriminator": user.discriminator,
-            "created date": user.created_at.strftime('%d %b. %Y'),
-            "joined date": user.joined_at.strftime('%d %b. %Y'),
+            "created date": user.created_at.strftime("%d %b. %Y"),
+            "joined date": user.joined_at.strftime("%d %b. %Y"),
         }.items():
-            embed.add_field(name=f'> {name}', value=f'`{value}`')
+            embed.add_field(name=f"> {name}", value=f"`{value}`")
 
         if user.activity is not None:
-            embed.add_field(name=f'> Activity', value=user.activity.name)
+            embed.add_field(name=f"> Activity", value=user.activity.name)
 
         if len(user.roles) < 50:
-            roles = ", ".join([f"{role.name}" for role in user.roles[:0: -1]])
+            roles = ", ".join([f"{role.name}" for role in user.roles[:0:-1]])
         else:
             roles = "too many roles"
 
         embed.add_field(
             name="> roles (%i)" % (len(user.roles) - 1),
             value=f"```c\n{roles} ```",
-            inline=False
+            inline=False,
         )
 
         await ctx.send(embed=embed)
 
-    @commands.command(
-        name='bot',
-        brief="Display the bot information"
-    )
+    @commands.command(name="bot", brief="Display the bot information")
     async def info_bot(self, ctx):
-        """ Display the bot information """
+        """Display the bot information"""
         _embed = self.bot.embed(
-            title=f'{self.bot.user.name} Bot Information',
-            description='\n'.join((
-                'This bot have been created, coded and is owned by Circuit#5585 and Sigmanificient#3301',
-                self.bot.user.created_at.strftime("> **Creation date** : %A %d %B %Y at %H:%M")
-            ))
+            title=f"{self.bot.user.name} Bot Information",
+            description="\n".join(
+                (
+                    "This bot have been created, coded and is owned by Circuit#5585 and Sigmanificient#3301",
+                    self.bot.user.created_at.strftime(
+                        "> **Creation date** : %A %d %B %Y at %H:%M"
+                    ),
+                )
+            ),
         )
 
         for key, val in {
-            'Python': python_version(),
-            'Discord': discord.__version__,
-            'Commands': len(self.bot.commands),
-            'Extensions': len(self.bot.cogs)
+            "Python": python_version(),
+            "Discord": discord.__version__,
+            "Commands": len(self.bot.commands),
+            "Extensions": len(self.bot.cogs),
         }.items():
-            _embed.add_field(name=key, value=f'> `{val}`', inline=True)
+            _embed.add_field(name=key, value=f"> `{val}`", inline=True)
 
         await ctx.send(embed=_embed)
 
