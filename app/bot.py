aaf0e96cbd758d3e2477a3570b749dee7eef42ea
Sigmanificient <edhyjox@gmail.com>
2021-05-25 22:06:45 +0200

    cogs loaded automatically

--- app/bot.py
+++ app/bot.py
@@ -4,35 +4,35 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from config import MCODING_SERVER
-
 load_dotenv()
-
-EXTENSIONS = [
-    "app.cogs.donor_role"
-]
 
 
 class Bot(commands.Bot):
+
     def __init__(self):
         super().__init__(command_prefix="!")
 
-        for ext in EXTENSIONS:
-            self.load_extension(ext)
+        for filename in os.listdir("cogs"):
+            if filename.endswith('py'):
+                self.load_extension(f"cogs.{filename[:-3]}")
+                print('-', filename)
 
     def run(self):
         return super().run(os.getenv("TOKEN"))
 
-    async def on_command_error(self, ctx: commands.Context, err: Exception):
-        await ctx.send(err)
+    async def on_command_error(self, ctx: commands.Context, error: Exception):
+        await ctx.send(repr(error))
 
     async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
+
         if not message.guild:
             return
-        if message.guild.id != MCODING_SERVER:
+
+        if message.guild.id != self.get_cog("DonorRole").mcoding_server_id:
             return
+
         await self.process_commands(message)
 
     async def on_ready(self):
