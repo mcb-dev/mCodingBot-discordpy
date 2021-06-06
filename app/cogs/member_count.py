3767eedd31c02338fc188dcfaa4f659f1cf2603c
Circuit <55721663+CircuitSacul@users.noreply.github.com>
2021-06-06 14:07:54 -0400

    Styling, sort imports.

--- app/cogs/member_count.py
+++ app/cogs/member_count.py
@@ -1,8 +1,8 @@
 from typing import TYPE_CHECKING
+from math import log
 
 from config import MEMBER_COUNT_CHANNEL, MCODING_SERVER
 from discord.ext import commands, tasks
-from math import log
 
 if TYPE_CHECKING:
     from app.bot import Bot
@@ -26,6 +26,7 @@ class MemberCount(commands.Cog):
         channel = self.bot.get_channel(MEMBER_COUNT_CHANNEL)
         guild = self.bot.get_guild(MCODING_SERVER)
         await channel.edit(name=f"Members: {self.get_member_count(guild)} ({guild.member_count})")
+
 
 def setup(bot: "Bot"):
     bot.add_cog(MemberCount(bot))
