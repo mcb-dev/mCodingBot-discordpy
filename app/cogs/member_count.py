de0b2556ca28af2f32308527c36ad674881c360c
trag1c <77130613+trag1c@users.noreply.github.com>
2021-06-06 18:11:17 +0200

    Update member_count.py

--- app/cogs/member_count.py
+++ app/cogs/member_count.py
@@ -14,21 +14,11 @@ class MemberCount(commands.Cog):
         self.update_member_count.start()
 
     def get_member_count(self, guild):
-        superscript = {
-            "0": "⁰", "1": "¹", "2": "²", "3": "³", "4": "⁴",
-            "5": "⁵", "6": "⁶", "7": "⁷", "8": "⁸", "9": "⁹",
-            ".": "ˑ"
-        }
         mc = guild.member_count
         mc_log = round(log(mc, 2), 3)
         if mc_log % 1 == 0:
             mc_log = int(mc_log)
-        mc_log_str = str(mc_log)
-
-        # Comment these 2 out to have 2^8.19 instead of 2⁸ˑ¹⁹
-        mc_log_str = f"\b{''.join([superscript[i] for i in mc_log_str])}"
-
-        return f"2^{mc_log_str}"
+        return f"2^{str(mc_log)}"
 
     @tasks.loop(minutes=10)
     async def update_member_count(self):
